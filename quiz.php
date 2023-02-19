eee9636eff4e6f850e473b3e40faf3644b19de83
junaid-mij <junath57@gmail.com>
2023-02-19 19:02:15 +0600

    New Feature

--- quiz.php
+++ quiz.php
@@ -20,29 +20,29 @@
 
 // echo "\n";
 
+//What is the output of this code?
+$numbers = array(1, 2, 3, 4, 5);
+$sum = 0;
+foreach ($numbers as $number){
+    if ($number % 2 == 0) {
+    continue;
+    }
+$sum += $number;
+}
+echo $sum;
+
+echo "\n";
+
+
 // //What is the output of this code?
-// $numbers = array(1, 2, 3, 4, 5);
-// $sum = 0;
-// foreach ($numbers as $number){
-//     if ($number % 2 == 0) {
-//     continue;
+// function strFunc($str){
+//     if (strlen($str) == 0) {
+//     return "";
+//     }else {
+//     return strFunc(substr($str, 1)) . substr($str, 0, 1);
+//     echo strFunc("hello");
 //     }
 // }
-// $sum += $number;
-// echo $sum;
-
-// echo "\n";
-
-
-//What is the output of this code?
-function strFunc($str){
-    if (strlen($str) == 0) {
-    return "";
-    }else {
-    return strFunc(substr($str, 1)) . substr($str, 0, 1);
-    echo strFunc("hello");
-    }
-}
 
 // //What is the output of this code?
 // $foo = function ($x) { return $x * 2;};
